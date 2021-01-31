bfa77ba689b9331724dee12aaddad291ab9ac071
Shaqqour <mshaqqourm@gmail.com>
2021-01-31 11:54:28 -0800

    Iterates through the movies and get each movie name to store it in an array

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,5 +5,10 @@ class Scraper
     url = "https://editorial.rottentomatoes.com/guide/essential-movies-to-watch-now/"
     html = open(url)
     doc = Nokogiri::HTML(html)
-    content = doc.css("div .row.countdown-item")
+    movies = doc.css("div .row.countdown-item")
+
+    movies_names = []
+    movies.each_with_index do |movie, idx|
+        movies_names << movie.css("div h2 a").text.strip
+    end
 end
