13138c230045373cc6e4353d2f4ff891ae6b3a33
Shaqqour <mshaqqourm@gmail.com>
2021-01-31 10:31:20 -0800

    Gets the web page and selects the contents we need

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,4 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 class Scraper
+    url = "https://editorial.rottentomatoes.com/guide/essential-movies-to-watch-now/"
+    html = open(url)
+    doc = Nokogiri::HTML(html)
+    content = doc.css("div .row.countdown-item")
 end
